11c4d09bc88f659b12d723800ec00927fd492b02
LeeCoder <curomame@naver.com>
2022-10-13 08:49:35 +0900

    ➕ ADD : 도균님 oauth test page

---
+++ pages/dk/index.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+function index() {
+
+  const [testUrl, setTestUrl] = useState("")
+
+  const onChangeUrl = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setTestUrl(e.target.value)
+  }
+
+  return (
+    <>
+    <input type="text" value={testUrl} onChange={(e) => onChangeUrl(e)}/>
+      <button onClick={()=>window.open(testUrl)} >이동하기</button>
+    </>
+  )
+}
+
+export default index

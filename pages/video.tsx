b7034fbc5a86135a2e1b64dfca47334602e3e2c4
curomame <curomame@naver.com>
2022-11-11 09:02:21 +0900

    🔧fix : vedio redirect 패치

--- pages/video.tsx
+++ pages/video.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function video() {
+
+  useEffect(() => {
+
+    window.location.replace("https://www.youtube.com/watch?v=qjYs0yMr-dI")
+
+  },[])
+
   return (
     <div>video</div>
   )
